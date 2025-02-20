e74c8bf07e64db48ef1a36701938c8a24afbd7cf
Laxmi Bamnale <laxmibamnale@gmail.com>
2025-02-20 12:13:11 +0530

    Initial commit of Redmine Careers Plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_careers do
+  name 'Redmine Careers Plugin'
+  author 'Laxmi Bamnale'
+  description 'A Redmine plugin for managing career opportunities and job postings.'
+  version '0.0.1'
+  url 'https://github.com/laxmi-27/redmine_careers'
+  author_url 'https://github.com/laxmi-27'
+end
